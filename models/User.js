63dfb22a620b051f8904bed4a7c785cc90f84307
wongpakho814 <wongpakho814@gmail.com>
2022-12-14 17:25:10 +0800

    Implemented models for User

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, Types } = require("mongoose");
+
+// Code referenced from https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+var validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please fill in a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = userSchema;
